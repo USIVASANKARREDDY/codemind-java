21e71fb68555a037fab02c9b953c74f855310caa
UDUMULA SIVA SANKAR REDDY <96323993+USIVASANKARREDDY@users.noreply.github.com>
2022-06-21 19:53:35 +0530

    import java.util.Scanner; class avg {     public static void main(String args[])     {         char ch;         Scanner sc=new Scanner(System.in);         ch=sc.next().charAt(0);         int x=(int)ch;         System.out.printf("%d",x);     } }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class avg
+{
+    public static void main(String args[])
+    {
+        char ch;
+        Scanner sc=new Scanner(System.in);
+        ch=sc.next().charAt(0);
+        int x=(int)ch;
+        System.out.printf("%d",x);
+    }
+}
