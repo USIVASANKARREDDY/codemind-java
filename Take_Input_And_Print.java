c63c8fbd67af2579e0e1ed3f8146401e74b5f866
UDUMULA SIVA SANKAR REDDY <96323993+USIVASANKARREDDY@users.noreply.github.com>
2022-06-21 19:58:31 +0530

    import java.util.Scanner; class avg {     public static void main(String args[])     {         String a;         Scanner sc=new Scanner(System.in);         a=sc.nextLine();         System.out.printf(a);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class avg
+{
+    public static void main(String args[])
+    {
+        String a;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextLine();
+        System.out.printf(a);
+    }
+}
